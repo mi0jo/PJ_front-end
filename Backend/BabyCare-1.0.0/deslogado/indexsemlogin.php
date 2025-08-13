865579f4474da5819278bc86e5728b8a18b41604
Milena <sutelmilena@gmail.com>
2025-08-12 23:28:25 -0300

    todos os navbard das pasta deslogado alterados para um require

--- Backend/BabyCare-1.0.0/deslogado/indexsemlogin.php
+++ Backend/BabyCare-1.0.0/deslogado/indexsemlogin.php
@@ -42,57 +42,10 @@
 
 
         <!-- Navbar start -->
-        <div class="container-fluid border-bottom bg-light wow fadeIn" data-wow-delay="0.1s">
-            <div class="container topbar bg-primary d-none d-lg-block py-2" style="border-radius: 0 40px">
-                <div class="d-flex justify-content-between">
-                    <div class="top-info ps-2">
-                        <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#" class="text-white">bleedwithdignity@gmail.com</a></small>
-                    </div>
-                    <div class="top-link pe-2">  
-                        <a href="" class="btn btn-light btn-sm-square rounded-circle"><i class="fab fa-instagram text-secondary"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="container px-0">
-                <nav class="navbar navbar-light navbar-expand-xl py-3">
-                    <a href="index.html" class="navbar-brand"><h1 class="text-primary display-6">BleedWith<span class="text-secondary">Dignity</span></h1></a>
-                    <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                        <span class="fa fa-bars text-primary"></span>
-                    </button>
-                     <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <div class="navbar-nav d-flex flex-row mx-auto align-items-center">
-                        <a href="index.html" class="nav-item nav-link mx-2 active">Tela Inicial</a>
-                        <div class="nav-item dropdown mx-2">
-                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Educação Sexual</a>
-                            <div class="dropdown-menu m-0 bg-secondary rounded-0">
-                                <a href="dignidademenstrual.html" class="dropdown-item">O que é Dignidade Menstrual?</a>
-                                <a href="saudesexual.html" class="dropdown-item">Saúde Sexual e Reprodutiva</a>
-                                <a href="produtos.html" class="dropdown-item">Produtos Menstruais para Todos</a>
-                                <a href="tiposdeabsorventes.html" class="dropdown-item">Tipos de Absorventes</a>
-                                <a href="ciclomenstrual.html" class="dropdown-item">Ciclo Menstrual</a>
-                                <a href="saude.html" class="dropdown-item">Saúde e Higiene Menstrual</a>
-                                <a href="sintomas.html" class="dropdown-item">Sintomas e Mudanças Menstruais</a>
-                                <a href="sustentabilidade.html" class="dropdown-item">Sustentabilidade Ambiental</a>
-                                <a href="pobrezamenstrual.html" class="dropdown-item">Pobreza Menstrual</a>
-                            </div>
-                        </div>
-                        <a href="calendario.html" class="nav-item nav-link mx-2">Calendário Menstrual</a>
-                        <a href="forum.html" class="nav-item nav-link ">Fórum</a>
-                        <a href="contact.html" class="nav-item nav-link mx-2">Contato</a>
-                                   </div>
-                       <div class="d-flex me-4">
-    <div id="phone-tada" class="d-flex align-items-center justify-content-center">
-        
-    </div>
+        <?php
+   require("../include/navbardeslogado.php");
+   ?>
    
-</div>
-<button class="btn-search btn btn-primary btn-md-square rounded-circle me-2" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search text-white"></i></button>
-<button class="btn btn-primary btn-md-square rounded-circle" data-bs-toggle="modal" data-bs-target="#profileModal"><i class="fas fa-user text-white"></i></button>
-                    </div>
-                </nav>
-            </div>
-        </div>
-
         <!-- Modal Perfil Start -->
 <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="profileModalLabel" aria-hidden="true">
     <div class="modal-dialog">
