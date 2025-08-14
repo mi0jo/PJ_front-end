f589e6c1c85642d334199a45596b26afbc4d1fce
Milena <sutelmilena@gmail.com>
2025-08-14 11:14:36 -0300

    as referencias alteradas para include na pasta de deslogado

--- Backend/BabyCare-1.0.0/deslogado/calendario.php
+++ Backend/BabyCare-1.0.0/deslogado/calendario.php
@@ -194,26 +194,11 @@
 
     </script>
     
-    <!-- Modal de Login Necessário -->
-    <div class="modal fade" id="loginRequiredModal" tabindex="-1" aria-labelledby="loginRequiredModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header bg-primary text-white">
-                    <h5 class="modal-title" id="loginRequiredModalLabel">Acesso Restrito</h5>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body text-center py-4">
-                    <i class="fas fa-lock fa-4x text-primary mb-3"></i>
-                    <h4 class="mb-3">Você precisa estar logado</h4>
-                    <p>Para acessar o Calendário Menstrual, faça login ou crie uma conta.</p>
-                </div>
-                <div class="modal-footer justify-content-center">
-                    <button type="button" class="btn btn-secondary me-2" data-bs-dismiss="modal">Mais tarde</button>
-                    <a href="login.php" class="btn btn-primary">Fazer Login</a>
-                </div>
-            </div>
-        </div>
-    </div>
+   
+       <?php
+      require("../include/modallogindeslogado.php");
+
+   ?>
 
         <!-- Back to Top -->
         <a href="#" class="btn btn-primary border-3 border-primary rounded-circle back-to-top"><i class="fa fa-arrow-up"></i></a>   
