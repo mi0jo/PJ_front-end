d9d0da10a2de26e53ba6557af41c7565af173296
Milena <sutelmilena@gmail.com>
2025-08-23 14:29:37 -0300

    include da bibliotexa java feito

--- Backend/BabyCare-1.0.0/deslogado/calendario.php
+++ Backend/BabyCare-1.0.0/deslogado/calendario.php
@@ -7,30 +7,12 @@
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="" name="keywords">
         <meta content="" name="description">
-
-        <!-- Google Web Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@600;700&family=Montserrat:wght@200;400;600&display=swap" rel="stylesheet"> 
-
-        <!-- Icon Font Stylesheet -->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
+ <?php
+   require("../include/referenciashead.php");
+   ?>
+       
         <!-- FullCalendar CSS -->
         <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css' rel='stylesheet' />
-
-        <!-- Libraries Stylesheet -->
-        <link href="../lib/animate/animate.min.css" rel="stylesheet">
-        <link href="../lib/lightbox/css/lightbox.min.css" rel="stylesheet">
-        <link href="../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-        <!-- Customized Bootstrap Stylesheet -->
-        <link href="../css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Template Stylesheet -->
-        <link href="../css/style.css" rel="stylesheet">
-
        
     </head>
 
@@ -197,28 +179,16 @@
    
        <?php
       require("../include/modallogindeslogado.php");
-
+ require("../include/bibliotecajava.php");
    ?>
 
-        <!-- Back to Top -->
-        <a href="#" class="btn btn-primary border-3 border-primary rounded-circle back-to-top"><i class="fa fa-arrow-up"></i></a>   
-
-        <!-- JavaScript Libraries -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="../lib/wow/wow.min.js"></script>
-        <script src="../lib/easing/easing.min.js"></script>
-        <script src="../lib/waypoints/waypoints.min.js"></script>
-        <script src="../lib/lightbox/js/lightbox.min.js"></script>
-        <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
+       
         
         <!-- FullCalendar JS -->
         <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js'></script>
         <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/locales/pt-br.js'></script>
 
-        <!-- Template Javascript -->
-        <script src="../js/main.js"></script>
-        
+      
     
     </body>
 </html>
